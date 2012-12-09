a8504a7413f3fd1ca6d7a8535f29c12c537517be
arlus <arlus@arlus.(none)>
2012-12-10 01:12:02 +0300

    changes to settings

--- settings.py
+++ settings.py
@@ -110,7 +110,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'shopcart.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
