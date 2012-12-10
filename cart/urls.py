c94b8c72e41a8f343f8ef5e5f094c29a24c35a69
Arlus <arlusishmael@gmail.com>
2012-12-10 09:32:40 +0300

    some changes in views

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,6 @@
 #cart urls...included in the root projects urls
 from django.conf.urls.defaults import *
-from shopcart.cart import views
+import views
 
 urlpatterns = patterns('',
 (r'^$', views.show_cart, { 'template_name': 'cart/cart.html' }, 'show_cart'),
